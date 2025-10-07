a21d2757d3081f6fe9afa1cc0ef65a3cf553f720
Zeeshan Nasir <z.nasir@umenit.com>
2025-10-07 21:26:07 +0530

    adding axios

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ utils/client.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const httpClient = axios.create({
+  baseURL: "/api",
+  // baseURL: "https://devwk.blueraiz.com/",
+});
+
+httpClient.interceptors.request.use((config) => {
+  const token = ''
+  const refresh_token = ''
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.refresh_token = `${refresh_token}`;    
+  }
+  return config;
+});
+
+
+const graphqlClient = axios.create({
+  baseURL: "/api/graphql/", // Will be rewritten to https://devwk.blueraiz.com/graphql by Next.js
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Attach Authorization token if available
+graphqlClient.interceptors.request.use((config) => {
+  const token = ''
+  const refresh_token = ''
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.refresh_token = `${refresh_token}`;
+  }
+  return config;
+});
+
+export{ graphqlClient, httpClient };
