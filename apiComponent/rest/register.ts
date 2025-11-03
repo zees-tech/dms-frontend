9fdcd6a8f946a1f8b631b8aad40c12a3a7ea70de
Zeeshan Nasir <z.nasir@umenit.com>
2025-11-03 19:53:37 +0530

    registering fixing

--- apiComponent/rest/register.ts
+++ apiComponent/rest/register.ts
@@ -2,13 +2,14 @@ import { httpClient } from "@/utils/client";
 import { RegisterResponse } from "@/types/auth";
 import { UserRole } from "@/types/auth";
 
-export async function register(username: string, email: string, password: string, role: UserRole): Promise<RegisterResponse> {
+export async function register(username: string, email: string, password: string, role: UserRole, department: string|null): Promise<RegisterResponse> {
   try {
     const response = await httpClient.post<RegisterResponse>("/api/Auth/register", {
         name: username,
         email: email,
         password: password,
-        role: 'user'
+        role: role,
+        department: department,
     });
     return response.data;
   } catch (error) {
